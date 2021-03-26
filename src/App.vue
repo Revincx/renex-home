b366825a7d1899088f325068347e9d83ce6b8f97
Revincx <revincx@outlook.com>
2021-03-26 17:56:27 +0800

    Inital Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Corner/>
+  <Central/>
+</template>
+
+<script>
+import Central from "./components/Central.vue";
+import Corner from "./components/Corner.vue";
+
+export default {
+  name: 'App',
+  components: {
+    Central,
+    Corner
+  }
+}
+</script>
+
+<style>
+
+body{
+  background: #FFE4E1;
+}
+
+</style>
---
+++ src/components/Central.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="central">
+    <p>renex.me</p>
+    <p>The Next Revincx Site</p>
+  </div>
+</template>
+
+<script>
+export default {
+name: "center"
+}
+</script>
+
+<style scoped>
+
+.central{
+  display: inline-block;
+  margin-top: 50vh;
+  transform: translateY(-50%);
+}
+
+p{
+  color: #FA8072;
+  font-size: 32px;
+  font-weight: bold;
+  letter-spacing: 3px;
+  font-family: Yellow-Peas,serif;
+}
+
+</style>
